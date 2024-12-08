5e80e2d9a8740f22bb4743a638236554c3d7ddb2
DINGBOE-EKOMI-ROMEO-CHRIST <lekid397@gmail.com>
2024-12-08 16:50:20 +0100

    Ajout de la deuxieme page

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -44,5 +44,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function chirps(): HasMany
+
+    {
+
+        return $this->hasMany(Chirp::class);
+
     }
 }
